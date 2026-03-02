2eb70357072531f1ab4e6f6be34aedde96feea3e
pavi2803 <pavi2468kuk@gmail.com>
2026-03-01 19:31:30 -0800

    updates

--- get_token_personal.py
+++ get_token_personal.py
@@ -1,16 +1,17 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import json
 
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.compose',
     'https://www.googleapis.com/auth/gmail.send'
 ]
 
-flow = InstalledAppFlow.from_client_secrets_file(
-    'credentials.json', SCOPES)
-creds = flow.run_local_server(port=0)
+flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+creds = flow.run_local_server(port=0, access_type='offline', prompt='consent')  # <--- key changes
 
-print("\n" + "="*50)
-print("COPY THIS TOKEN:")
-print("="*50)
-print(creds.to_json())
-print("="*50)
+# Save token for future runs
+with open('token.json', 'w') as f:
+    f.write(creds.to_json())
+
+print("Token saved to token.json. You can now use it in your deployed app.")
